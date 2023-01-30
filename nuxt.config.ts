6712ec48d1d0b8a9afb5059850a05824e359fcf4
David Glaeseman <backgroundcolor@gmail.com>
2023-01-30 13:08:01 -0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            title: 'Extending Configs is Fun!!',
+            meta: [
+                { name: 'description', content: 'I am using the extends feature in nuxt 3!' }
+            ]
+        }
+    }
 })
